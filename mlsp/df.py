583a49104baf73edcaa0223ac315570027cd1156
EmpireDemocratiqueDuPoulpe <alexis.lecomte@supinfo.com>
2022-06-01 10:16:49 +0200

    mlsp.df.missing_values() prints with percentages

--- mlsp/df.py
+++ mlsp/df.py
@@ -15,14 +15,16 @@ def first_look(df):
 
 def missing_values(df, keep_zeros=True):
     data_count = df.shape[0] * df.shape[1]
-    missing = df.isna().sum().sort_values(ascending=False)
+    missing = missing_df = df.isna().sum()
 
     if not keep_zeros:
-        missing = missing[missing > 0]
+        missing_df = missing_df[missing_df > 0]
+
+    missing_df = missing_df.sort_values(ascending=False).apply(lambda m: f"{m} ({round((m * 100) / df.shape[0], 2)}%)")
 
     print((
         f"Missing values: {Fore.LIGHTGREEN_EX}{round((missing.sum() / data_count) * 100, 2)}%\n"
-        f"{Fore.WHITE}{Style.DIM}{missing}"
+        f"{Fore.WHITE}{Style.DIM}{missing_df}"
     ))
 
 
